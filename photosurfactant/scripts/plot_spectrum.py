54c6465abc394f33d7b4e6f9389d5fef68fcdff5
Niall Oswald <niall.oswald@hotmail.co.uk>
2024-11-14 17:04:07 +0000

    Feat: Add spectrum plotting script

---
+++ photosurfactant/scripts/plot_spectrum.py
@@ -0,0 +1,63 @@
+#! /usr/bin/env python
+from photosurfactant.parameters import Parameters, PlottingParameters
+from photosurfactant.leading_order import LeadingOrder
+from photosurfactant.first_order import FirstOrder
+from photosurfactant.fourier import fourier_series_coeff
+from photosurfactant.utils import (
+    parameter_parser,
+    plot_parser,
+    leading_order_parser,
+    first_order_parser,
+)
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+import numpy as np
+
+
+def plot_spectrum():  # noqa: D103
+    parser = ArgumentParser(
+        description="Plot the first order surfactant concentrations.",
+        formatter_class=ArgumentDefaultsHelpFormatter,
+    )
+    parameter_parser(parser)
+    plot_parser(parser)
+    leading_order_parser(parser)
+    first_order_parser(parser)
+    args = parser.parse_args()
+
+    root_index = args.root_index
+    func = lambda x: 1.0  # noqa: E731
+
+    params = Parameters.from_dict(vars(args))
+    plot_params = PlottingParameters.from_dict(vars(args))
+
+    omega, func_coeffs = fourier_series_coeff(func, params.L, plot_params.wave_count)
+    func_coeffs = np.ones_like(func_coeffs, dtype=np.complex128)
+
+    # Solve leading order problem
+    leading = LeadingOrder(params, root_index)
+
+    # Solve first order problem
+    first = FirstOrder(omega, func_coeffs, params, leading)
+
+    # Figure setup
+    plt = plot_params.plt
+
+    # Plot the spectrum
+    plt.figure(figsize=(8, 6))
+    plt.plot(
+        omega[plot_params.wave_count :],
+        np.abs(first.S_f()[plot_params.wave_count + 1 :]),
+        "k-",
+    )
+    plt.yscale("log")
+    plt.xlabel(r"$k_n$")
+    plt.ylabel(r"$|S^{(n)}|$")
+    plt.tight_layout()
+
+    if plot_params.save:
+        plt.savefig(
+            plot_params.path + f"spectrum{plot_params.label}.png",
+            dpi=300,
+        )
+    else:
+        plt.show()
