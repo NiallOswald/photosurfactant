1c448784c5fb7326e1f2dcdbdfbeefaa5466d28e
Niall Oswald <niall.oswald@hotmail.co.uk>
2025-01-13 19:26:07 +0000

    Fix: Normalize mollifier

--- photosurfactant/functions.py
+++ photosurfactant/functions.py
@@ -1,6 +1,7 @@
 """Example light intensity and surface perturbations."""
 
 import numpy as np
+import scipy as sp
 
 
 def gaussian(x):
@@ -15,8 +16,11 @@ def smoothed_square(x, delta):
     return 0.5 * (np.tanh((x + 1) / delta) - np.tanh((x - 1) / delta))
 
 
-def mollifier(x, delta):
-    if abs(x) < 1:
-        return np.exp(-(delta**2) / (delta**2 - x**2))
-    else:
-        return 0.0
+def mollifier(delta):
+    def _(x):
+        if abs(x) < delta:
+            return np.exp(-(delta**2) / (delta**2 - x**2))
+        else:
+            return 0.0
+
+    return lambda x: _(x) / sp.integrate.quad(_, -1.0, 1.0)[0]
