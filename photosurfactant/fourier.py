a6270b76beaf68bc0f0c3b623688fd6a7febf0e0
Niall Oswald <niall.oswald@hotmail.co.uk>
2025-01-13 17:51:02 +0000

    Feat: Add convolution in Fourier space

--- photosurfactant/fourier.py
+++ photosurfactant/fourier.py
@@ -27,3 +27,25 @@ def fourier_series_coeff(func, L, N):
     omega = np.concatenate([omega_full[:N], omega_full[N + 1 :]])
 
     return omega, f_full
+
+
+def convolution_coeff(f, g, L, N):
+    """Calculate the first 2*N+1 Fourier series coeff. of a convolution.
+
+    Given periodic functions f(x), g(x) with period 2L, this function returns the
+    coefficients {c0,c1,c2,...} such that:
+
+    f(x) * g(x) ~= sum_{k=-N}^{N} c_k * exp(i*2*pi*k*x/L)
+
+    where we define c_{-n} = complex_conjugate(c_{n}).
+
+    :param f: A periodic function, a callable like f(x).
+    :param g: A periodic function, a callable like f(x).
+    :param L: Half the period of the function f, so that f(-L)==f(L).
+    :param N: The function will return the first N + 1 Fourier coeff.
+    """
+    omega, f_full = fourier_series_coeff(f, L, N)
+    _, g_full = fourier_series_coeff(g, L, N)
+    f_conv_full = 2 * L * f_full * g_full
+
+    return omega, f_conv_full
--- photosurfactant/functions.py
+++ photosurfactant/functions.py
@@ -3,11 +3,20 @@
 import numpy as np
 
 
-def laser_square(x, delta):
-    """Return a smoothed square wave."""
+def gaussian(x):
+    return np.exp(-4 * x**2)
+
+
+def square_wave(x):
+    return float(abs(x) < 1)
+
+
+def smoothed_square(x, delta):
     return 0.5 * (np.tanh((x + 1) / delta) - np.tanh((x - 1) / delta))
 
 
-def laser_gauss(x):
-    """Return a Gaussian-like function."""
-    return np.exp(-4 * x**2)
+def mollifier(x, delta):
+    if abs(x) < 1:
+        return np.exp(-(delta**2) / (delta**2 - x**2))
+    else:
+        return 0.0
