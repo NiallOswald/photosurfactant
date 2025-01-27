bab001b439fc757de9bc5f26831ca85e3ac5d2f5
Niall Oswald <niall.oswald@hotmail.co.uk>
2025-01-27 17:31:07 +0000

    Fix: Remove duplicate labels

--- photosurfactant/scripts/plot_all.sh
+++ photosurfactant/scripts/plot_all.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 echo "Plotting leading order..."
-plot_leading_order -s --path "./figures/" --label "blue" --grid_size 1000 --usetex --format "pdf"
+plot_leading_order -s --path "./figures/" --grid_size 1000 --usetex --format "pdf"
 
 echo "Plotting first order: laser pointer..."
-plot_first_order -s --path "./figures/laser-point/" --label "laser" --grid_size 1000 --usetex --format "pdf"
+plot_first_order -s --path "./figures/laser-point/" --grid_size 1000 --usetex --format "pdf"
 echo "Plotting first order: mixing..."
-plot_first_order --func "np.cos(2 * np.pi * x / L)" -s --path "./figures/mixing/" --label "mixing" --grid_size 1000 --usetex --format "pdf"
+plot_first_order --func "np.cos(2 * np.pi * x / L)" -s --path "./figures/mixing/" --grid_size 1000 --usetex --format "pdf"
 echo "Plotting first order: inverse problem..."
-plot_first_order --func "square_wave(x)" --mollify --problem inverse --wave_count 30 -s --path "./figures/inverse/" --label "inverse" --grid_size 1000 --usetex --format "pdf"
+plot_first_order --func "square_wave(x)" --mollify --problem inverse --wave_count 30 -s --path "./figures/inverse/" --grid_size 1000 --usetex --format "pdf"
 echo "Plotting first order: power spectrum..."
 plot_spectrum -s --path "./figures/" --grid_size 1000 --usetex --format "pdf"
 
