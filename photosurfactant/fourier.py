ba15c19547209e06dbe6460a61a9442e7ea0d616
Niall Oswald <niall.oswald@hotmail.co.uk>
2025-02-18 12:52:48 +0000

    Feat: Return only positive Fourier coefficients

--- photosurfactant/fourier.py
+++ photosurfactant/fourier.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def fourier_series_coeff(func, L, N):
-    """Calculate the first 2*N+1 Fourier series coeff. of a periodic function.
+    """Calculate the first N+1 Fourier series coeff. of a periodic function.
 
     Given a periodic function f(x) with period 2L, this function returns the
     coefficients {c0,c1,c2,...} such that:
@@ -15,22 +15,17 @@ def fourier_series_coeff(func, L, N):
 
     :param func: The periodic function, a callable like f(x).
     :param L: Half the period of the function f, so that f(-L)==f(L).
-    :param N: The function will return the first 2N + 1 Fourier coeff.
+    :param N: The function will return the first N + 1 Fourier coeff.
     """
     xx = np.linspace(-L, L, 2 * N, endpoint=False)
     f_coeffs = np.fft.rfft(np.array([func(x) for x in xx])) / len(xx)
     f_coeffs *= np.exp(1.0j * np.pi * np.arange(N + 1))  # Shift frequency domain
-    f_full = np.concatenate([[f_coeffs[0]], f_coeffs[1:][::-1].conj(), f_coeffs[1:]])
 
-    omega_full = np.arange(-N, N + 1) * np.pi / L
-    # Excluding the zero frequency
-    omega = np.concatenate([omega_full[:N], omega_full[N + 1 :]])
-
-    return omega, f_full
+    return np.arange(0, N + 1) * np.pi / L, f_coeffs
 
 
 def convolution_coeff(f, g, L, N):
-    """Calculate the first 2*N+1 Fourier series coeff. of a convolution.
+    """Calculate the first N+1 Fourier series coeff. of a convolution.
 
     Given periodic functions f(x), g(x) with period 2L, this function returns the
     coefficients {c0,c1,c2,...} such that:
