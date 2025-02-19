1941e0b61c613d64c88305620cef6480821e617a
Niall Oswald <niall.oswald@hotmail.co.uk>
2025-02-19 16:01:41 +0000

    Feat: Update function used for inverse figures

--- photosurfactant/scripts/plot_all.sh
+++ photosurfactant/scripts/plot_all.sh
@@ -7,7 +7,7 @@ plot_first_order -s --path "./figures/laser-point/" --grid_size 1000 --usetex --
 echo "Plotting first order: mixing..."
 plot_first_order --func "np.cos(2 * np.pi * x / L)" -s --path "./figures/mixing/" --grid_size 1000 --usetex --format "pdf"
 echo "Plotting first order: inverse problem..."
-plot_first_order --func "square_wave(x)" --mollify --problem inverse --wave_count 30 -s --path "./figures/inverse/" --grid_size 1000 --usetex --format "pdf"
+plot_first_order --func "np.cos(np.pi * x / L) + np.cos(2 * np.pi * x / L) / 2 + np.cos(3 * np.pi * x / L) / 3" --problem inverse --wave_count 3 -s --path "./figures/inverse/" --grid_size 1000 --usetex --format "pdf"
 echo "Plotting first order: power spectrum..."
 plot_spectrum -s --path "./figures/" --grid_size 1000 --usetex --format "pdf"
 
