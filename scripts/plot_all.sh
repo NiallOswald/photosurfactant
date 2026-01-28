013751ce15cad799b9045be622b0e58eca0ec6c9
Niall Oswald <niall.oswald@hotmail.co.uk>
2026-01-28 14:47:26 +0000

    chore: update plot_all script

--- scripts/plot_all.sh
+++ scripts/plot_all.sh
@@ -1,16 +1,19 @@
 #! /bin/bash
 echo "Plotting leading order..."
-plot_leading_order --intensities -6.0 2.46598000289 --log --count 13 -s --path "./figures/leading/" --grid_size 1000 --usetex --format "pdf"
-plot_leading_order --interface --limits --intensities -6.0 2.0 --log --count 1000 -s --path "./figures/leading/" --grid_size 1000 --usetex --format "pdf"
+plot_leading_order --intensities -6.0 2.0 --log --count 13 -s --path "./figures/leading/" --usetex --format "pdf"
+plot_leading_order --interface --limits --intensities -6.0 2.0 --log --count 1000 -s --path "./figures/leading/" --usetex --format "pdf"
 
 echo "Plotting first order: laser pointer..."
-plot_first_order -s --path "./figures/laser-point/" --grid_size 1000 --usetex --format "pdf"
+plot_first_order -s --path "./figures/laser-point/" --usetex --format "pdf"
 echo "Plotting first order: mixing..."
-plot_first_order --func "np.cos(2 * np.pi * x / params.L)" -s --path "./figures/mixing/" --grid_size 1000 --usetex --format "pdf"
+plot_first_order --func "np.cos(2 * np.pi * x / params.L)" -s --path "./figures/mixing/" --usetex --format "pdf"
 echo "Plotting first order: inverse problem..."
-plot_first_order --func "1e-3 * (-np.cos(2 * np.pi * x / params.L) + np.cos(4 * np.pi * x / params.L) / 2 + np.cos(6 * np.pi * x / params.L) / 3)" --problem inverse --wave_count 7 -s --path "./figures/inverse/" --grid_size 1000 --usetex --format "pdf"
-echo "Plotting first order: power spectrum..."
-plot_spectrum -s --path "./figures/" --grid_size 1000 --usetex --format "pdf"
+plot_first_order --func "np.cos(np.pi * x / params.L) + np.cos(2 * np.pi * x / params.L) / 2 - np.cos(3 * np.pi * x / params.L) / 3" --problem inverse --wave_count 4 -s --path "./figures/inverse/" --usetex --format "pdf"
+echo "Plotting first order: inverse problem..."
+plot_first_order --func "(np.sinh(1) / (np.cosh(1) - np.cos(np.pi * x / params.L)) - 1) / 2" --problem inverse --wave_count 30 -s --path "./figures/inverse-adv/" --usetex --format "pdf"
+
+echo "Plotting first order: sweeping parameters..."
+plot_sweep -s --path "./figures/sweep/" --usetex --format "pdf"
 
 echo "Plotting convergence...:"
-plot_error
+plot_error -s --path "" --usetex --format "pdf"
